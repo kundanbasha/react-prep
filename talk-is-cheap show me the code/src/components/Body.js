a57dc1d00907f3822789aab88c3e6363b0bcb40e
Alam Basha <alambashak@narayanagroup.com>
2024-01-21 00:20:52 +0530

    ep:5 Let's get hooked, code commit

--- talk-is-cheap show me the code/src/components/Body.js
+++ talk-is-cheap show me the code/src/components/Body.js
@@ -1,14 +1,29 @@
+import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
+  const arr = useState(resList);
+  const listOfRestaurants = arr[0];
+  const setListofRestaurants = arr[1];
+
   return (
     <div className="body">
       <div className="filter">
-        <button className="filter-btn">Top Rated Restaurants</button>
+        <button
+          onClick={() => {
+            const filteredRestaurants = listOfRestaurants.filter(
+              (restaurant) => restaurant.avgRating > 4
+            );
+            setListofRestaurants(filteredRestaurants);
+          }}
+          className="filter-btn"
+        >
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
